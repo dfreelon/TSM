2463d2708845fd56e4dff0122f1d94e1574f773b
Aaron Schumacher <ajschumacher@gmail.com>
2015-02-22 18:34:06 -0500

    add tests for reading files
    noticed issue #3 for strange null byte scenarios

--- test_tsm.py
+++ test_tsm.py
@@ -1,6 +1,7 @@
 import tsm
 import unittest
 import unittest.mock as mock
+import io
 
 
 class TestLoadData(unittest.TestCase):
@@ -19,6 +20,31 @@ class TestLoadData(unittest.TestCase):
         self.assertIsNot(original, loaded)
         # Test copy is deep:
         self.assertIsNot(original[2], loaded[2])
+
+    @mock.patch('builtins.open')
+    def test_calls_open_with_default_args(self, mock_open):
+        result = tsm.load_data('path')
+        mock_open.assert_called_once_with('path', 'r',
+                                          encoding='utf-8', errors='replace')
+
+    @mock.patch('builtins.open')
+    def test_calls_open_with_encoding(self, mock_open):
+        result = tsm.load_data('path', enc='other')
+        mock_open.assert_called_once_with('path', 'r', encoding='other',
+                                          errors='replace')
+
+    def test_returns_list_of_lists_from_csv(self):
+        fake_file = io.StringIO('one,two,three\n1,2,3\n')
+        with mock.patch('builtins.open', return_value=fake_file):
+            result = tsm.load_data('path')
+        self.assertEqual(result, [['one', 'two', 'three'], ['1', '2', '3']])
+
+    def test_strips_null_bytes_from_csv(self):
+        # This doesn't check for the case when a file ends "blah\n\0"
+        fake_file = io.StringIO('\0one,t\0wo,three\n1\0,2,3\0\n')
+        with mock.patch('builtins.open', return_value=fake_file):
+            result = tsm.load_data('path')
+        self.assertEqual(result, [['one', 'two', 'three'], ['1', '2', '3']])
 
 
 if __name__ == '__main__':
