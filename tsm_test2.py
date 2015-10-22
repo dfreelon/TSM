ce0ecbb8057de7a581679ddab2010eca6f88ad20
Deen Freelon <dfreelon@users.noreply.github.com>
2015-10-22 17:16:20 -0400

    Update tsm_test2.py

--- tsm_test2.py
+++ tsm_test2.py
@@ -11,10 +11,26 @@ edgelist_A = tsm.t2e(tweet_file_A)
 edgelist_B = tsm.t2e(tweet_file_B)
 top_communities_A = tsm.get_top_communities(edgelist_A)
 top_communities_B = tsm.get_top_communities(edgelist_B)
-ei_indices_A = tsm.calc_ei(top_communities_A,edgelist_A,'ON')
-ei_indices_B = tsm.calc_ei(top_communities_B,edgelist_B,'ON')
+ei_indices_A = tsm.calc_ei(top_communities_A,edgelist_A)
+ei_indices_B = tsm.calc_ei(top_communities_B,edgelist_B)
 top_rts_A = tsm.get_top_rts(tweet_file_A,top_communities_A)
 top_rts_B = tsm.get_top_rts(tweet_file_B,top_communities_B)
-tsm.match_communities(top_communities_A,top_communities_B)
+community_matches = tsm.match_communities(top_communities_A,top_communities_B)
+intermediaries_A = get_intermediaries(top_communities_A,edgelist_A)
+intermediaries_B = get_intermediaries(top_communities_B,edgelist_B)
+top_hashtags_A = get_top_hashtags(tweet_file_A,top_communities_A)
+top_hashtags_B = get_top_hashtags(tweet_file_B,top_communities_B)
+top_links_A = get_top_links(tweet_file_A,top_communities_A)
+top_links_B = get_top_links(tweet_file_B,top_communities_B)
+grid_A = shared_ties_grid(ei_indices_A,calc_propor=True)
+grid_B = shared_ties_grid(ei_indices_B,calc_propor=True)
 
 print("Now feel free to inspect the variables...")
+''' Use the following code to inspect grid_A and grid_B:
+
+for line in grid_A:
+    print(line)
+
+for line in grid_B:
+    print(line)
+'''
